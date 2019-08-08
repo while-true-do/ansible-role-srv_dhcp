7c929fcdfd4bec81fce3d48dd980aae7a2b5d7ac
daniel-wtd <daniel@while-true-do.io>
2019-08-08 22:56:51 +0200

    fix: testing for packages does not work

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,12 +8,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_dhcp_package(host):
-    pkg = host.package('dhcp')
-
-    assert pkg.is_installed
-
-
 def test_dhcp_service(host):
     srv = host.service('dhcpd')
 
